8175595a4a7f5b6105c3f51f13a6897c4c305b28
Gord Harris <gordopolis@yahoo.com>
2011-08-03 19:41:58 -0700

    non-functional whitespace cleanup

--- Roman Numerals 2/helper.js
+++ Roman Numerals 2/helper.js
@@ -7,28 +7,36 @@ function RomanNumeral() {
 	this.toRoman = function(value) {
 		if (value < 0) {
 			return null;
+			
 		} else if (value == 0) {
 			return "N";
+			
 		} else if (value < 4) {
 			val = "";
 			for (var i = 1; i <= value; i++) {
 				val = val + "I";
 			}
 			return val;
+			
 		} else if (value == 4) {
 			return "IV";
+			
 		} else if (value == 5) {
 			return "V";
+			
 		} else if (value < 9) {
 			val = "V";
 			for (var i = 6; i <= value; i++) {
 				val = val + "I";
 			}
 			return val;	
+			
 		} else if (value == 9) {
 			return "IX";
+			
 		} else if (value == 10) {
 			return "X";
+			
 		}
 	};
 	
