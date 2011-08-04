9c589fcb41042f502cf609cc910c702017ae83b6
Gord Harris <gordopolis@yahoo.com>
2011-08-03 20:04:08 -0700

    refactor

--- Roman Numerals 2/helper.js
+++ Roman Numerals 2/helper.js
@@ -4,12 +4,6 @@ function echoFunction(message) {
 
 function RomanNumeral() {
 	
-	letters = new Array();
-	letters[1] = "I";
-	letters[5] = "V";
-	letters[10] = "X";
-	letters[50] = "L";
-	
 	this.append = function(start, append, count) {
 		val = start;
 		for (var i = 1; i <= count; i++) {
@@ -18,30 +12,35 @@ function RomanNumeral() {
 		return val;
 	};
 	
+	var chars  = new Array("I", "V", "X", "L");
+	var values = new Array(1, 5, 10, 50);
+	
+	
 	this.toRoman = function(value) {
 		if (value < 0) {
 			return null;
 			
 		} else if (value == 0) {
 			return "N";
+		}
+		
+		for (var i = 0; i < values.length; i++) {
+			if (values[i] == value) {
+				return chars[i];
+			}
+		}
 			
-		} else if (value < 4) {
+		if (value < 4) {
 			return this.append("", "I", value);
 			
 		} else if (value == 4) {
 			return "IV";
-			
-		} else if (value == 5) {
-			return "V";
 			
 		} else if (value < 9) {
 			return this.append("V", "I", value - 5);
 			
 		} else if (value == 9) {
 			return "IX";
-			
-		} else if (value == 10) {
-			return "X";
 			
 		}
 	};
