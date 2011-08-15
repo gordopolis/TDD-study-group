bff5bf4dd1232148581626d8dd814ba83d1927a7
Gord Harris <gordopolis@yahoo.com>
2011-08-15 06:49:27 -0700

    simple change

--- foobar/HelperSpec.js
+++ foobar/HelperSpec.js
@@ -2,8 +2,8 @@ describe("helper.js", function() {
 	
 	describe("echoFunction", function() {
 		it("test output is equal to input", function() {
-			var messageResult = echoFunction("hello world");
-			expect(messageResult.toString()).toEqual("Received: hello world");
+			var messageResult = echoFunction("hello world xxx");
+			expect(messageResult.toString()).toEqual("Received: hello world xxx");
 		});
 	});
 	
